47b7117c75aa5b0424da04a53a8a5070589d61cf
jakob. spiel .køhler <jakobspielkohler@live.dk>
2022-05-15 13:47:45 +0200

    broker example works

---
+++ MqttBrokerStandaloneExsample/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using TopicFramework;
+using TopicFramework.Events;
+using MQTTnet;
+using Microsoft.Extensions.Logging;
+using System.Reflection;
+using TopicFramework.Mqtt;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Starting Mqtt broker.....");
+
+IConfiguration config = new ConfigurationBuilder()
+                        .AddEnvironmentVariables()
+                        .AddJsonFile("appsettings.json")
+                        .AddUserSecrets<Program>()
+                        .Build();
+
+MqttServiceSettings.DebugLog = true;
+
+HostBuilder hostBuilder = new HostBuilder();
+
+hostBuilder.ConfigureLogging((context, builder) => builder.AddConsole());
+
+hostBuilder.ConfigureServices(services =>
+{
+    services.AddSingleton<MqttFactory>();
+    services.AddTopicFrameWork(Assembly.GetEntryAssembly());
+    services.AddTfMqttBrokerService(options =>
+    {
+        options.WithDefaultEndpointPort(1883);
+    });
+});
+
+var app = hostBuilder.Build();
+
+app.UseTopicFramework();
+
+app.Run();
--- TopicFramework/TopicFramworkExtensions.cs
+++ TopicFramework/TopicFramworkExtensions.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using TopicFramework.Events;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
 
 namespace TopicFramework
 {
@@ -30,6 +31,22 @@ namespace TopicFramework
 
         public static WebApplication? UseTopicFramework(this WebApplication? application)
         {
+            var instance = application.Services.GetRequiredService<TopicInstance>();
+
+            if (instance == null)
+                throw new InvalidOperationException("Sevice not loaded. Use AddTopicFramework() to load");
+
+            instance.LoadServiceProvider(application.Services);
+
+            return application;
+        }
+
+        public static IHost? UseTopicFramework(this IHost? application)
+        {
+
+            if (application == null)
+                throw new InvalidOperationException("Application is NULL");
+
             var instance = application.Services.GetRequiredService<TopicInstance>();
 
             if (instance == null)
