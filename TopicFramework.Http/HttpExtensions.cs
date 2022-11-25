cb542aad928361eeafc3e6da17a23f25fbd18302
jakob. spiel .køhler <jakobspielkohler@live.dk>
2022-11-25 22:52:01 +0100

    Added authorization to webapi

--- TopicFramework.Http/HttpExtensions.cs
+++ TopicFramework.Http/HttpExtensions.cs
@@ -10,10 +10,17 @@ namespace TopicFramework.Http
 {
     public static class HttpExtensions
     {
-
-        public static WebApplication AddTopicFrameWorkHttpApi(this WebApplication application)
+        
+        public static WebApplication AddTopicFrameWorkHttpApi(this WebApplication application,Authorize = false)
         {
             application.MapPost("/tf/publish", new RequestDelegate(ApiHandlers.PublishRequest));
+
+            return application;
+        }
+
+        public static WebApplication AddTopicFrameWorkHttpApiWithAuthorization(this WebApplication application,string scopes)
+        {
+            application.MapPost("/tf/publish", new RequestDelegate(ApiHandlers.PublishRequest)).WithAuthorization(scopes);
 
             return application;
         }
