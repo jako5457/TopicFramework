54aa53d96cdd7c77e3c6210c10338be11065ae34
jako5457 <jako5457@gmail.com>
2022-11-27 22:18:23 +0100

    fixed extensions

--- TopicFramework.Http/HttpExtensions.cs
+++ TopicFramework.Http/HttpExtensions.cs
@@ -11,16 +11,16 @@ namespace TopicFramework.Http
     public static class HttpExtensions
     {
         
-        public static WebApplication AddTopicFrameWorkHttpApi(this WebApplication application,Authorize = false)
+        public static WebApplication AddTopicFrameWorkHttpApi(this WebApplication application,bool Authorize = false)
         {
             application.MapPost("/tf/publish", new RequestDelegate(ApiHandlers.PublishRequest));
 
             return application;
         }
 
-        public static WebApplication AddTopicFrameWorkHttpApiWithAuthorization(this WebApplication application,string scopes)
+        public static WebApplication AddTopicFrameWorkHttpApiWithAuthorization(this WebApplication application,params string[] PolicyNames)
         {
-            application.MapPost("/tf/publish", new RequestDelegate(ApiHandlers.PublishRequest)).WithAuthorization(scopes);
+            application.MapPost("/tf/publish", new RequestDelegate(ApiHandlers.PublishRequest)).RequireAuthorization(PolicyNames);
 
             return application;
         }
